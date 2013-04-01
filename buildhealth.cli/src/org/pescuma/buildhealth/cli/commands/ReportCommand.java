b08ea7d3f11a80206c6da9dffbf574fc95e4471d
Ricardo Pescuma Domenecci <ricardo@pescuma.org>
2013-04-01 22:38:08 +0100

    Use colors in console

--- buildhealth.cli/src/org/pescuma/buildhealth/cli/commands/ReportCommand.java
+++ buildhealth.cli/src/org/pescuma/buildhealth/cli/commands/ReportCommand.java
@@ -2,14 +2,19 @@ package org.pescuma.buildhealth.cli.commands;
 
 import io.airlift.command.Command;
 
+import org.fusesource.jansi.Ansi;
+import org.fusesource.jansi.Ansi.Color;
+import org.fusesource.jansi.AnsiConsole;
 import org.pescuma.buildhealth.analyser.coverage.CoverageAnalyser;
 import org.pescuma.buildhealth.analyser.diskusage.DiskUsageAnalyser;
 import org.pescuma.buildhealth.analyser.loc.LOCAnalyser;
 import org.pescuma.buildhealth.analyser.staticanalysis.StaticAnalysisAnalyser;
 import org.pescuma.buildhealth.analyser.unittest.UnitTestAnalyser;
 import org.pescuma.buildhealth.cli.BuildHealthCliCommand;
+import org.pescuma.buildhealth.core.BuildStatus;
 import org.pescuma.buildhealth.core.Report;
 import org.pescuma.buildhealth.core.ReportFormater;
+import org.pescuma.buildhealth.core.ReportFormater.Outputer;
 
 @Command(name = "report", description = "Report the status of the current build")
 public class ReportCommand extends BuildHealthCliCommand {
@@ -24,7 +29,48 @@ public class ReportCommand extends BuildHealthCliCommand {
 		buildHealth.addAnalyser(new DiskUsageAnalyser());
 		
 		Report report = buildHealth.generateReportSummary();
-		System.out.println(new ReportFormater().format(report));
+		
+		ReportFormater reportFormater = new ReportFormater(new ReportFormater.Outputer() {
+			private Ansi ansi;
+			
+			@Override
+			public Outputer start() {
+				ansi = Ansi.ansi();
+				return this;
+			}
+			
+			@Override
+			public Outputer append(String text, BuildStatus status) {
+				ansi.fg(toColor(status)).a(text).reset();
+				return this;
+			}
+			
+			private Color toColor(BuildStatus status) {
+				switch (status) {
+					case Good:
+						return Ansi.Color.GREEN;
+					case SoSo:
+						return Ansi.Color.YELLOW;
+					case Problematic:
+						return Ansi.Color.RED;
+					default:
+						throw new IllegalStateException();
+				}
+			}
+			
+			@Override
+			public Outputer append(String text) {
+				ansi.a(text);
+				return this;
+			}
+			
+			@Override
+			public String toString() {
+				return ansi.toString();
+			}
+		});
+		
+		AnsiConsole.out.print(reportFormater.format(report));
 	}
 	
 }
--- buildhealth.core/src/org/pescuma/buildhealth/core/ReportFormater.java
+++ buildhealth.core/src/org/pescuma/buildhealth/core/ReportFormater.java
@@ -1,6 +1,5 @@
 package org.pescuma.buildhealth.core;
 
-import java.util.Arrays;
 import java.util.List;
 
 public class ReportFormater {
@@ -8,40 +7,82 @@ public class ReportFormater {
 	private static final String NO_DATA = "No data to generate report";
 	private static final String PREFIX = "    ";
 	
-	public String format(Report report) {
-		if (report == null)
-			return NO_DATA;
-		
-		StringBuilder result = new StringBuilder();
-		
-		if (report.getName().equals("Build")) {
-			result.append(createSummaryLine(report)).append("\n");
-			append(result, report.getChildren(), PREFIX);
+	private final Outputer outputer;
+	
+	public ReportFormater() {
+		this(new Outputer() {
+			private StringBuilder result = new StringBuilder();
 			
-		} else {
-			append(result, Arrays.asList(report), "");
-		}
-		
-		return result.toString();
+			@Override
+			public Outputer start() {
+				result = new StringBuilder();
+				return this;
+			}
+			
+			@Override
+			public Outputer append(String text) {
+				result.append(text);
+				return this;
+			}
+			
+			@Override
+			public Outputer append(String text, BuildStatus status) {
+				result.append(text);
+				return this;
+			}
+			
+			@Override
+			public String toString() {
+				return result.toString();
+			}
+		});
+	}
+	
+	public ReportFormater(Outputer outputer) {
+		this.outputer = outputer;
 	}
 	
 	public String createSummaryLine(Report report) {
 		if (report == null)
 			return NO_DATA;
 		
-		return "Your build is " + createTitle(report.getStatus());
+		outputer.start();
+		appendSummaryLine(report);
+		return outputer.toString();
 	}
 	
-	private void append(StringBuilder result, List<Report> reports, String prefix) {
+	private void appendSummaryLine(Report report) {
+		if (report.getName().equals("Build"))
+			outputer.append("Your build is ").append(createTitle(report.getStatus()), report.getStatus());
+		else
+			outputer.append(report.getName()).append(": ").append(report.getValue(), report.getStatus());
+		
+		String description = report.getDescription();
+		if (!description.isEmpty())
+			outputer.append(" [").append(description).append("]");
+	}
+	
+	public String format(Report report) {
+		if (report == null)
+			return NO_DATA;
+		
+		outputer.start();
+		
+		appendSummaryLine(report);
+		outputer.append("\n");
+		
+		append(report.getChildren(), PREFIX);
+		
+		return outputer.toString();
+	}
+	
+	private void append(List<Report> reports, String prefix) {
 		for (Report report : reports) {
-			result.append(prefix);
-			result.append(report.getName()).append(": ").append(report.getValue());
-			String description = report.getDescription();
-			if (!description.isEmpty())
-				result.append(" [").append(description).append("]");
-			result.append("\n");
+			outputer.append(prefix);
+			appendSummaryLine(report);
+			outputer.append("\n");
 			
-			append(result, report.getChildren(), prefix + PREFIX);
+			append(report.getChildren(), prefix + PREFIX);
 		}
 	}
 	
@@ -56,6 +97,17 @@ public class ReportFormater {
 			default:
 				throw new IllegalStateException();
 		}
+	}
+	
+	public static interface Outputer {
+		Outputer start();
+		
+		Outputer append(String text);
+		
+		Outputer append(String text, BuildStatus status);
+		
+		@Override
+		String toString();
 	}
 	
 }
