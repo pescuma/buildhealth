908179c85a972505215fa9aef6122b31b5e06228
Ricardo Pescuma Domenecci <ricardo@pescuma.org>
2013-05-04 17:55:38 +0100

    Added JapexExtractor

---
+++ buildhealth.core/src/org/pescuma/buildhealth/extractor/performance/JapexExtractor.java
@@ -0,0 +1,59 @@
+package org.pescuma.buildhealth.extractor.performance;
+
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.pescuma.buildhealth.core.BuildData;
+import org.pescuma.buildhealth.extractor.BaseXMLExtractor;
+import org.pescuma.buildhealth.extractor.PseudoFiles;
+
+import com.google.common.base.Strings;
+
+public class JapexExtractor extends BaseXMLExtractor {
+	
+	public JapexExtractor(PseudoFiles files) {
+		super(files);
+	}
+	
+	@Override
+	protected void extractDocument(String filename, Document doc, BuildData data) {
+		checkRoot(doc, "testSuiteReport");
+		
+		extractSuite(filename, doc.getRootElement(), data);
+	}
+	
+	private void extractSuite(String filename, Element suite, BuildData data) {
+		String type = suite.getChildTextTrim("resultUnit", suite.getNamespace());
+		
+		if ("ms".equalsIgnoreCase(type))
+			type = "ms";
+		else if ("tps".equalsIgnoreCase(type))
+			type = "runsPerS";
+		else if ("mbps".equalsIgnoreCase(type))
+			// TODO Handle japex.InputFile
+			type = "runsPerS";
+		else
+			// What to do?
+			return;
+		
+		for (Element driver : suite.getChildren("driver", suite.getNamespace()))
+			extractDriver(driver, type, data);
+	}
+	
+	private void extractDriver(Element driver, String type, BuildData data) {
+		String name = driver.getAttributeValue("name", "<no name>");
+		
+		String descr = driver.getChildTextTrim("description");
+		if (!Strings.isNullOrEmpty(descr))
+			name = descr;
+		
+		for (Element testCase : driver.getChildren("testCase", driver.getNamespace()))
+			extractTestCase(testCase, name, type, data);
+	}
+	
+	private void extractTestCase(Element testCase, String driver, String type, BuildData data) {
+		String name = testCase.getAttributeValue("name", "<no name>");
+		double val = Double.parseDouble(testCase.getChildTextTrim("resultValue", testCase.getNamespace()));
+		
+		data.add(val, "Performance", "Java", "Japex", type, driver, name);
+	}
+}
---
+++ buildhealth.core/tests/org/pescuma/buildhealth/extractor/japex/JapexExtractorTest.java
@@ -0,0 +1,30 @@
+package org.pescuma.buildhealth.extractor.japex;
+
+import static org.mockito.Matchers.*;
+import static org.mockito.Mockito.*;
+
+import java.io.File;
+import java.io.InputStream;
+
+import org.junit.Test;
+import org.pescuma.buildhealth.extractor.BaseExtractorTest;
+import org.pescuma.buildhealth.extractor.PseudoFiles;
+import org.pescuma.buildhealth.extractor.performance.JapexExtractor;
+
+public class JapexExtractorTest extends BaseExtractorTest {
+	
+	@Test
+	public void test6PassedGeral() {
+		InputStream stream = load("japex.xml");
+		
+		JapexExtractor extractor = new JapexExtractor(new PseudoFiles(stream));
+		
+		extractor.extractTo(table, tracker);
+		
+		verify(tracker).onStreamProcessed();
+		verify(tracker, never()).onFileProcessed(any(File.class));
+		
+		assertTable(2, 0.175 + 0.315, table.filter("Performance"));
+	}
+	
+}
