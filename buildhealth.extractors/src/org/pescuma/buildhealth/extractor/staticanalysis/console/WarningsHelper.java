5ccc444fe466e2eae78ef643db08cafa01a7de0f
Ricardo Pescuma Domenecci <ricardo@pescuma.org>
2014-03-31 13:53:54 -0300

    Created Compiler errors category and removed dups

--- buildhealth.extractors/src/org/pescuma/buildhealth/extractor/staticanalysis/console/WarningsHelper.java
+++ buildhealth.extractors/src/org/pescuma/buildhealth/extractor/staticanalysis/console/WarningsHelper.java
@@ -11,7 +11,9 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Collection;
+import java.util.HashSet;
 
+import org.apache.commons.lang.StringUtils;
 import org.pescuma.buildhealth.core.BuildData;
 import org.pescuma.buildhealth.utils.Location;
 
@@ -21,13 +23,21 @@ class WarningsHelper {
 			throws IOException, ParsingCanceledException {
 		Collection<FileAnnotation> anns = parser.parse(new InputStreamReader(input));
 		
+		// Remove dups
+		anns = new HashSet<FileAnnotation>(anns);
+		
 		for (FileAnnotation ann : anns) {
 			LineRange r = ann.getLineRanges().iterator().next();
 			Location loc = Location.create(ann.getFileName(), r.getStart(), ann.getColumnStart(), r.getEnd(),
 					ann.getColumnEnd());
 			
+			String category = (ann.getPriority() == Priority.HIGH ? "Compiler errors" : "Compiler warnings");
+			
+			if (!StringUtils.isBlank(ann.getCategory()))
+				category += "/" + ann.getCategory().replace('\\', '/');
+			
 			data.add(1, "Static analysis", detectLanguage(ann.getFileName()), name, Location.toFormatedString(loc),
-					"Compiler warnings", ann.getMessage(), toSeverity(ann.getPriority()));
+					category, ann.getMessage(), toSeverity(ann.getPriority()));
 		}
 	}
 	
